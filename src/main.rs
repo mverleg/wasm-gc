c0ffee2e1a95bfbc18ad03ff5f9e5276a59e6e56
Mark <mverleg.noreply@gmail.com>
2024-05-18 10:46:15 +0200

    Try to convert wat to wasm from Rust

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+use std::{env, fs};
+use ::wabt::Wat2Wasm;
+
 fn main() {
-    println!("Hello, world!");
+    run(env::args().next())
+}
+
+fn run(wat_file: Option<String>) {
+    let wat_pth = wat_file
+        .unwrap_or_else(|| "main.wat".to_owned());
+    let wat_data = match fs::read_to_string(wat_pth) {
+        Ok(data) => data,
+        Err(err) => panic!("could not read wat file '{}', error: {err}", &wat_pth),
+    };
+    let wasm_binary = Wat2Wasm::new()
+        .canonicalize_lebs(false)
+        .write_debug_names(true)
+        .convert(wat_data).unwrap();
+
 }

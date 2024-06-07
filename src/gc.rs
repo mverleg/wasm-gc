c0ffee028653c0097d93bc287592b06f247f3ff1
Mark <mverleg.noreply@gmail.com>
2024-06-08 00:08:40 +0200

    State management for GC

--- src/gc.rs
+++ src/gc.rs
@@ -1,4 +1,5 @@
 use std::cell::RefCell;
+use std::ops::Sub;
 
 #[derive(Debug)]
 struct StackHeader {}
@@ -11,18 +12,46 @@ struct OldHeapHeader {}
 
 #[derive(Debug)]
 struct GcState {
-
+    stack_capacity: WordSize,
+    young_capacity: WordSize,
+    old_capacity: WordSize,
+    stack_len: WordSize,
+    young_side: Side,
+    young_len: WordSize,
+    old_len: WordSize,
 }
 
-#[derive(Debug, Clone, Copy)]
+#[derive(Debug, Clone, Copy, PartialEq)]
+struct ByteSize(u32);
+
+#[derive(Debug, Clone, Copy, PartialEq)]
 struct WordSize(u32);
 
-#[derive(Debug, Clone, Copy)]
+#[derive(Debug, Clone, Copy, PartialEq)]
 struct Pointer(u32);
+
+impl Sub for Pointer {
+    type Output = ByteSize;
+
+    fn sub(self, rhs: Self) -> Self::Output {
+        ByteSize(rhs.0 - self.0)
+    }
+}
+
+#[derive(Debug, Clone, Copy, PartialEq)]
+enum Side { Left, Right }
 
 thread_local! {
     static STATE: RefCell<GcState> = RefCell::new(GcState {
+        stack_capacity: WordSize(1024),
+        young_capacity: WordSize(16384),
+        old_capacity: WordSize(16384),
 
+        stack_len: WordSize(0),
+        young_side: Side::Left,
+        young_len: WordSize(0),
+        old_len: WordSize(0),
+        //TODO @mark: translate this to top addresse and derive lengths from them
     });
 }
 
@@ -66,6 +95,6 @@ mod tests {
     fn alloc_data_on_heap() {
         let orig = alloc_heap(WordSize(0), WordSize(2), false);
         let subsequent = alloc_heap(WordSize(0), WordSize(2), false);
-        assert!(subsequent - orig == 12);
+        assert_eq!(subsequent - orig, ByteSize(12));
     }
 }

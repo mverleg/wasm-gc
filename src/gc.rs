c0ffee430ce225c543a40167bb53e07ec62feb18
Mark <mverleg.noreply@gmail.com>
2024-06-08 00:18:22 +0200

    Newtype wrappers

--- src/gc.rs
+++ src/gc.rs
@@ -1,5 +1,11 @@
-use std::cell::RefCell;
-use std::ops::Sub;
+use ::std::cell::RefCell;
+use ::std::mem::size_of;
+use ::std::ops::Sub;
+use ::std::ops::Add;
+use std::ops::Mul;
+
+type Addr = u32;
+const WORD_SIZE: Addr = 4;
 
 #[derive(Debug)]
 struct StackHeader {}
@@ -10,25 +16,51 @@ struct YoungHeapHeader {}
 #[derive(Debug)]
 struct OldHeapHeader {}
 
+const OFFSET: Pointer = Pointer(size_of::<GcState>() as u32 + WORD_SIZE);
+
 #[derive(Debug)]
 struct GcState {
     stack_capacity: WordSize,
-    young_capacity: WordSize,
+    young_side_capacity: WordSize,
     old_capacity: WordSize,
-    stack_len: WordSize,
+    stack_top: Pointer,
     young_side: Side,
-    young_len: WordSize,
-    old_len: WordSize,
+    young_top: Pointer,
+    old_top: Pointer,
+}
+
+impl GcState {
+    fn stack_len(&self) -> WordSize {
+        unimplemented!()
+    }
+
+    fn young_side(&self) -> Side {
+        unimplemented!()
+    }
+
+    fn young_len(&self) -> WordSize {
+        unimplemented!()
+    }
+
+    fn old_len(&self) -> WordSize {
+        unimplemented!()
+    }
 }
 
 #[derive(Debug, Clone, Copy, PartialEq)]
-struct ByteSize(u32);
+struct ByteSize(Addr);
 
 #[derive(Debug, Clone, Copy, PartialEq)]
-struct WordSize(u32);
+struct WordSize(Addr);
+
+impl WordSize {
+    fn bytes(self) -> ByteSize {
+        ByteSize(WORD_SIZE *  self.0)
+    }
+}
 
 #[derive(Debug, Clone, Copy, PartialEq)]
-struct Pointer(u32);
+struct Pointer(Addr);
 
 impl Sub for Pointer {
     type Output = ByteSize;
@@ -38,21 +70,40 @@ impl Sub for Pointer {
     }
 }
 
+impl Mul<u32> for ByteSize {
+    type Output = ByteSize;
+
+    fn mul(self, rhs: u32) -> Self::Output {
+        ByteSize(self.0 * rhs)
+    }
+}
+
+impl Add<ByteSize> for Pointer {
+    type Output = Pointer;
+
+    fn add(self, rhs: ByteSize) -> Self::Output {
+        Pointer(self.0 + rhs.0)
+    }
+}
+
 #[derive(Debug, Clone, Copy, PartialEq)]
 enum Side { Left, Right }
 
 thread_local! {
-    static STATE: RefCell<GcState> = RefCell::new(GcState {
-        stack_capacity: WordSize(1024),
-        young_capacity: WordSize(16384),
-        old_capacity: WordSize(16384),
-
-        stack_len: WordSize(0),
-        young_side: Side::Left,
-        young_len: WordSize(0),
-        old_len: WordSize(0),
-        //TODO @mark: translate this to top addresse and derive lengths from them
-    });
+    static STATE: RefCell<GcState> = {
+        let stack_capacity = WordSize(1024);
+        let young_side_capacity = WordSize(16384);
+        let old_capacity = WordSize(16384);
+        RefCell::new(GcState {
+            stack_capacity,
+            young_side_capacity,
+            old_capacity,
+            stack_top: OFFSET,
+            young_side: Side::Left,
+            young_top: OFFSET + stack_capacity.bytes(),
+            old_top: OFFSET + stack_capacity.bytes() + young_side_capacity.bytes() * 2,
+        })
+    }
 }
 
 pub fn alloc_heap(
